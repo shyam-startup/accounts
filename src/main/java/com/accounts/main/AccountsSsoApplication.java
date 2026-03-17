344af9d1827098e56d3ad62e2deb9432dceee645
shyam-startup <shyamsundar.startup@gmail.com>
2026-03-17 14:12:59 +0530

    Initial commit

---
+++ src/main/java/com/accounts/main/AccountsSsoApplication.java
@@ -0,0 +1,13 @@
+package com.accounts.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsSsoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/accounts/main/AccountsSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accounts.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
